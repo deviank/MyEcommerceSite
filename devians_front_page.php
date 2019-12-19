1f7d6342283de16ca4f1e03845f5173da62e7edd
Devian <deviank@live.co.za>
2019-12-19 14:34:25 +0200

    Adding additional products to multi array.

--- devians_front_page.php
+++ devians_front_page.php
@@ -4,24 +4,33 @@ $pictures = array('tire.png', 'oil.jpg', 'spark_plug.jpg',
     'thermostat.jpg', 'wiper_blade.jpg',
     'gasket.jpg', 'brake_pad.jpg');
 
-$products = array(array('CAR_TIR', 'Tires', 100,
-                        'CAR_OIL', 'Oil', 10,
-                        'CAR_SPRK', 'Spark Plugs', 4,
+$categories = array(array(array('CAR_TIR', 'Tires', 100),
+                          array('CAR_OIL', 'Oil', 10),
+                          array('CAR_SPRK', 'Spark Plugs', 4)
                         ),
-                   array('VAN_TIR', 'Tires', 100,
-                         'VAN_OIL', 'Oil', 10,
-                         'VAN_SPRK', 'Spark Plugs', 4,
+                    array(array('VAN_TIR', 'Tires', 100),
+                          array( 'VAN_OIL', 'Oil', 10),
+                          array('VAN_SPRK', 'Spark Plugs', 4),
                     ),
-                   array('BIKE_TIR', 'Tires', 100,
-                         'BIKE_OIL', 'Oil', 10,
-                         'BIKE_SPRK', 'Spark Plugs', 4,
+                    array(array('BIKE_TIR', 'Tires', 100),
+                          array('BIKE_OIL', 'Oil', 10),
+                          array('BIKE_SPRK', 'Spark Plugs', 4)
                     ),
-                   array('TRUCK_TIR', 'Tires', 100,
-                         'TRUCK_OIL', 'Oil', 10,
-                         'TRUCK_SPRK', 'Spark Plugs', 4,
+                    array(array('TRUCK_TIR', 'Tires', 100),
+                          array('TRUCK_OIL', 'Oil', 10),
+                          array('TRUCK_SPRK', 'Spark Plugs', 4)
                     ),
                   );
 
+for($layer = 0; $layer < 4; $layer++){
+    echo "Layer $layer<br />";
+    for($row = 0; $row < 3; $row++){
+        for($column = 0; $column < 3; $column++){
+            echo '|' . $categories[$layer][$row][$column];
+        }
+        echo '<br />';
+    }
+}
 
 ?>
 <html>
