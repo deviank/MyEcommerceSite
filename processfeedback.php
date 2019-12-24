df62096dabfd296586fc9106f22e48ee82720856
Devian <deviank@live.co.za>
2019-12-24 13:28:32 +0200

    Adding html feedback form.

--- processfeedback.php
+++ processfeedback.php
@@ -1,9 +1,32 @@
 <?php
 
-//create short variables names
+//create short variable names
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$feedback = trim($_POST['feedback']);
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$feedback = $_POST['feedback'];
+//set up some static information
+$toaddress = "forza9882@gmail.com";
 
-//setting up some static information
+$subject = "Feedback from web site";
+
+$mailcontent = "Customer name: ".$name."\n".
+    "Customer email: ".$email."\n".
+    "Customer comments:\n".$feedback."\n";
+
+$fromaddress = "From: webserver@example.com";
+
+//invoke mail() function to send mail
+mail($toaddress, $subject, $mailcontent, $fromaddress);
+
+?>
+<html>
+<head>
+    <title>Bob's Auto Parts - Feedback Submitted</title>
+</head>
+<body>
+<h1>Feedback submitted</h1>
+<p>Your feedback (shown below) has been sent.</p>
+<p><?php echo nl2br($mailcontent); ?> </p>
+</body>
+</html>
